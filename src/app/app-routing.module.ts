1ff408bf34648d7e3957cbc946a2e7f19083a741
GabrielHuanacoImpa <gabrielhuanaco@gmail.com>
2022-09-30 22:31:39 -0300

    estoy seguro de este commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
-const routes: Routes = [];
+import { FichaJugadorComponent } from './ficha/ficha.component';
+
+import { Pagina404Component } from './pagina404/pagina404.component';
+
+const routes: Routes = [
+  {path: 'servicios', component:ServiciosComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'mi-perfil', component:PerfilComponent},
+  {path: 'sobre-nosotros', component: AboutComponent},
+  {path: 'jugador/:nombre', component: FichaJugadorComponent}, 
+  {path: '**', component: Pagina404Component},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    
+    AppRoutingModule,
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  
+  declarations: [ AppComponent, Pagina404Component ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
